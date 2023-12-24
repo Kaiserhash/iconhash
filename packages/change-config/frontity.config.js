00f1e1a949fbd26c22bb29f7e98f01575505d334
js-senpai <fincheran@gmail.com>
2023-12-24 23:08:39 +0200

    test commit

--- packages/change-config/frontity.config.js
+++ packages/change-config/frontity.config.js
@@ -1,24 +1,24 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 export const webpack = ({ config, mode }) => {
-    if(process.env.NODE_ENV !== 'development'){
-        config.optimization = {
-            ...config.optimization,
-            minimize: true,
-            minimizer: [
-                new UglifyJsPlugin({
-                    test: /\.js(\?.*)?$/i,
-                }),
-            ],
-        }
-
-        config.plugins.push(
-            new CompressionPlugin({
-                algorithm: "gzip",
-                test: /\.js$|\.css$|\.html$/,
-                threshold: 10240,
-                minRatio: 0.8
-            }),
-        )
-    }
+    // if(process.env.NODE_ENV !== 'development'){
+    //     config.optimization = {
+    //         ...config.optimization,
+    //         minimize: true,
+    //         minimizer: [
+    //             new UglifyJsPlugin({
+    //                 test: /\.js(\?.*)?$/i,
+    //             }),
+    //         ],
+    //     }
+    //
+    //     config.plugins.push(
+    //         new CompressionPlugin({
+    //             algorithm: "gzip",
+    //             test: /\.js$|\.css$|\.html$/,
+    //             threshold: 10240,
+    //             minRatio: 0.8
+    //         }),
+    //     )
+    // }
 };
